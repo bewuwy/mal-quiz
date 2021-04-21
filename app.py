c31c5fb7e9479ed57afb9d2b163fa17b27036b34
bewu <bewuwy@gmail.com>
2021-04-21 21:48:27 +0200

    added more configuration
    added css, fonts and responsivness
    and added a finish page

--- app.py
+++ app.py
@@ -10,10 +10,14 @@ app.secret_key = environ.get("secret_key")
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    user = ""
+    if "user" in session:
+        user = session["user"]
+
+    return render_template("index.html", user=user)
 
 
-@app.route("/play")
+@app.route("/start")
 def play():
     user = request.args.get("user")
     if not user:
@@ -22,34 +26,46 @@ def play():
     r = requests.get(f"https://themes.moe/api/mal/{user}")
     anime_list = r.json()
 
-    questions = int(request.args.get("n")) * int(len(anime_list) > 10)
+    questions = int(request.args.get("n")) * int(len(anime_list) > int(request.args.get("n")))
     themes = []
+
+    statuses = []
+    for i in request.args:
+        if i.startswith("s"):
+            statuses.append(int(i[1]))
+
     while len(themes) != questions:
         anime = random.choice(anime_list)
 
         th = random.choice(anime["themes"])
-        if anime["watchStatus"] != 6 and th["themeType"].find("OP") > -1\
+        if anime["watchStatus"] in statuses and th["themeType"].find("OP") > -1\
                 and [th, [anime["name"], anime["malID"]]] not in themes:
             themes.append([th, [anime["name"], anime["malID"]]])
 
     session["themes"] = themes
+    session["user"] = user
     session["options"] = {}
     session["options"]["video"] = request.args.get("video") == "on"
 
-    return render_template("playstart.html")
+    return render_template("playstart.html", user=user)
 
 
 @app.route("/play/<n>")
 def quiz(n):
     n = int(n)
     if "themes" not in session or not session["themes"]:
-        return redirect("/play")
+        return redirect("/start")
 
     themes = session["themes"]
     if n >= len(themes):
-        return redirect(f"/play/{len(themes)-1}")
+        return redirect(f"/finish")
 
     th = themes[n]
 
     return render_template("play.html", src=th[0]["mirror"]["mirrorURL"], video=session["options"]["video"],
-                           animeTitle=th[1][0], songTitle=th[0]["themeName"], next=n+1)
+                           anime=th[1], songTitle=th[0]["themeName"], next=n+1)
+
+
+@app.route("/finish")
+def finish():
+    return render_template("finish.html")
